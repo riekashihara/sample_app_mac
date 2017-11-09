52409a26764e4bee960ca940f7fd0efadb1cb24b
樫原理映子 <kashiharaosamueiko@kashiharaosamueiko-no-MacBook.local>
2017-11-09 13:42:33 +0900

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,6 +8,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
  test "index as admin including pagination and delete links" do
    log_in_as(@admin)
+
    get users_path
    assert_template 'users/index'
    assert_select 'div.pagination'
